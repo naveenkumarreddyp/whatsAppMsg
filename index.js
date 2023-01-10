1d8e4cbf31292365f5b56e5ae5eae0de4f6ffd3f
naveenkumarreddyp <92851599+naveenkumarreddyp@users.noreply.github.com>
2023-01-10 12:12:01 +0530

    newCode

--- index.js
+++ index.js
@@ -8,74 +8,126 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.json());
 
-let verifyToken = "abcd";
-let accessToken = "EAAO9aDLEtwABAGVaIyMZCvz2Ha6Covx3BEfZCrActg5XpPHwloxMft9leurncO3EQvNgbqwuV51VjJPYEH6FkiXgw77OXPpznqx9brBtvEzOHlJSvu3l2TzZCZCU4qHwbzpD1RzcuJ2yDcjLpNMkcZALZCNTpsRV0DZAZAi0js15BLLiUD4CGGRX";
-
-// to verify the callback ul from dashboard side- cloud api side
-app.get("/webhook", (req, res) => {
-  let mode = req.query["hub.mode"];
-  let challenge = req.query["hub.challenge"];
-  let token = req.query["hub.verify_token"];
-  if (mode && token) {
-    if (mode === "subscribe" && token === verifyToken) {
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403); // forbidden
-    }
-  }
-});
-
-app.post("/webhook", async (req, res) => {
-  let body_param = req.body;
-  console.log("body_param", body_param)
-  if (body_param.object) {
-    console.log("object",body_param.object )
-    console.log("object1",body_param.entry )
-    console.log("object2",body_param.entry[0].changes )
-     console.log("object3",body_param.entry[0].changes[0].value.messages )
-    console.log("phno",body_param.entry[0].changes[0].value.metadata.phone_number_id )
-    console.log("from",body_param.entry[0].changes[0].value.messages[0].from )
-    console.log("msgbody",body_param.entry[0].changes[0].value.messages[0].text.body )
-    if (
-      body_param.entry &&
-      body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.messages &&
-      body_param.entry[0].changes[0].value.messages[0]
-    ) {
-      let phoneNumber =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msgBody = body_param.entry[0].changes[0].value.messages[0].text.body;
-      try {
-        console.log(phoneNumber, from, msgBody);
-        if (msgBody.toLowerCase() === "hi") {//
-          msgBody = "Hello Naveen";
+let mytoken = "abcd";
+let token = "EAAO9aDLEtwABAKbZBo7DiuT9KC434wZAZCmCT22KtiEZBLSbiBqGaVPZArZBqRl1q6053XkC0s3CSZAlDfW2gIsMBEEJr444y5eABwgZBcFnpuHwfnN3d3IGh0L7rPt1tZAEFr57w48YxC22k4mxb1FGKS5dy2hpdSxlEjwMQWDkXNkfCaDivKuHE";
+app.get("/webhook",(req,res)=>{
+   let mode=req.query["hub.mode"];
+   let challange=req.query["hub.challenge"];
+   let token=req.query["hub.verify_token"];
+    if(mode && token){
+        if(mode==="subscribe" && token===mytoken){
+            res.status(200).send(challange);
+        }else{
+            res.status(403);
         }
-        console.log(phoneNumber, from, msgBody);
-        let data = await axios({
-          method: "POST",
-          url: `https://graph.facebook.com/v15.0/${phoneNumber}/messages`,
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "content-type": "application/json",
-          },
-          data: {
-            messaging_product: "whatsapp",
-            to: from,
-            text: {
-              body: msgBody,
-            },
-          },
-        });
-//         console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
     }
-  }
-  res.status(200).send("OK");
 });
+
+app.post("/webhook",(req,res)=>{ //i want some 
+    let body_param=req.body;
+    console.log(JSON.stringify(body_param,null,2));
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("body param "+msg_body);
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v15.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Naveen, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
+            }
+    }
+
+});
+// to verify the callback ul from dashboard side- cloud api side
+// app.get("/webhook", (req, res) => {
+//   let mode = req.query["hub.mode"];
+//   let challenge = req.query["hub.challenge"];
+//   let token = req.query["hub.verify_token"];
+//   if (mode && token) {
+//     if (mode === "subscribe" && token === verifyToken) {
+//       console.log("WEBHOOK_VERIFIED");
+//       res.status(200).send(challenge);
+//     } else {
+//       res.sendStatus(403); // forbidden
+//     }
+//   }
+// });
+
+// app.post("/webhook", async (req, res) => {
+//   let body_param = req.body;
+//   console.log("body_param", body_param)
+//   if (body_param.object) {
+//     console.log("object",body_param.object )
+//     console.log("object1",body_param.entry )
+//     console.log("object2",body_param.entry[0].changes )
+//      console.log("object3",body_param.entry[0].changes[0].value.messages )
+//     console.log("phno",body_param.entry[0].changes[0].value.metadata.phone_number_id )
+//     console.log("from",body_param.entry[0].changes[0].value.messages[0].from )
+//     console.log("msgbody",body_param.entry[0].changes[0].value.messages[0].text.body )
+//     if (
+//       body_param.entry &&
+//       body_param.entry[0].changes &&
+//       body_param.entry[0].changes[0].value.messages &&
+//       body_param.entry[0].changes[0].value.messages[0]
+//     ) {
+//       let phoneNumber =
+//         body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//       let from = body_param.entry[0].changes[0].value.messages[0].from;
+//       let msgBody = body_param.entry[0].changes[0].value.messages[0].text.body;
+//       try {
+//         console.log(phoneNumber, from, msgBody);
+//         if (msgBody.toLowerCase() === "hi") {//
+//           msgBody = "Hello Naveen";
+//         }
+//         console.log(phoneNumber, from, msgBody);
+//         let data = await axios({
+//           method: "POST",
+//           url: `https://graph.facebook.com/v15.0/${phoneNumber}/messages`,
+//           headers: {
+//             Authorization: `Bearer ${accessToken}`,
+//             "content-type": "application/json",
+//           },
+//           data: {
+//             messaging_product: "whatsapp",
+//             to: from,
+//             text: {
+//               body: msgBody,
+//             },
+//           },
+//         });
+// //         console.log(data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     }
+//   }
+//   res.status(200).send("OK");
+// });
 
 // app.post("/webhook", async (req, res) => {
 //   let body_param = req.body;
