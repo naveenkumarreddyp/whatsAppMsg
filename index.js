08bc0bc7097c3bca12e16d736b3a05cd1cbbeced
naveenkumarreddyp <92851599+naveenkumarreddyp@users.noreply.github.com>
2023-01-10 10:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 
 let verifyToken = "abcd";
-let accessToken = "EAAO9aDLEtwABAAZAxdA09MX7xUS38HJSBSKTEZCV9g6XhZBdaf2U0IpcaXNNiAJroxW9XtPFGLDIYPjXe6k5f8LrRMI3w0cTknyGI05FQy8Kgi4PbomHmZABuhWlCoxlaZBzOZAkb4IJQZBWMESmhobgZBxp4Oc0EfmBYshllV7ZB1ZAjfrGGZCx6cz";
+let accessToken = "EAAO9aDLEtwABAMxWg5jaz8ZBSNiELTrnVLYueC9s59sCl5U0cIsr3f7dOYtCbQupYQIZBUyaodMLZCukrdOaDaJIVZAywDmKLpZAHkWAjwxvB1hJ1xZApYhRtfzMKSrtECR6lz2JnheWH8Anz4JLqhzW0oSbH3HRWeLh33qAmgShpG7IjyQYKomrvlHXmdmoF0IutjRHTpHwZDZD";
 
 // to verify the callback ul from dashboard side- cloud api side
 app.get("/webhook", (req, res) => {
